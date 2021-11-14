79d0ddbacf460bf5974158f5183bb2c98b433187
Michael Solano <mike.solano@gmail.com>
2021-11-13 23:43:02 -0600

    Improved responses and added some additional error handling.

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,51 @@
-from flask import jsonify, request
+from flask import jsonify, request, make_response
+from marshmallow import ValidationError
 
 from app.models import Book
 from app.schemas import BookSchema
 from run import app
 
+# create schemas objects
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
 
-@app.route('/api/environment', methods=["GET"])
+
+@app.route('/api/environment', methods=['GET'])
 def get_env():
-    return app.config['FLASK_ENV']
+    return make_response(app.config['FLASK_ENV'], 200)
 
 
-@app.route('/api/books', methods=["GET"])
+@app.route('/api/books', methods=['GET'])
 def get_books():
-    books_schema = BookSchema(many=True)
-    return jsonify(books_schema.dump(Book.query.all()))
+    return make_response(jsonify(books_schema.dump(Book.query.all())), 200)
+
+
+@app.route('/api/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = book_schema.dump(Book.query.get(id))
+    if not book:
+        return make_response({"message": "Book not found"}, 400)
+    else:
+        return make_response(jsonify(book), 200)
 
 
 @app.route('/api/books', methods=['POST'])
 def create_book():
-    book_schema = BookSchema()
-    data = book_schema.load(request.get_json())
+    try:
+        data = book_schema.load(request.get_json())
+    except ValidationError as e:
+        return make_response(jsonify({"messages": e.messages}), 400)
+
     book = Book(title=data['title'],
                 author_first_name=data['author_first_name'],
                 author_last_name=data['author_last_name'],
-                year=data['year'])
+                year=data['year'] if 'year' in data.keys() else None)
     result = book_schema.dump(book.create())
-    return jsonify(book_schema.dump(result))
 
+    return make_response(jsonify(book_schema.dump(result)), 201)
 
-@app.route('/api/books/<int:id>', methods=["GET"])
-def get_book(id):
-    book_schema = BookSchema()
-    return jsonify(book_schema.dump(Book.query.get(id)))
+# @app.rout('/api/books/<int:id>', methods=['PUT', 'PATCH'])
+# def update_book():
 
 
 
